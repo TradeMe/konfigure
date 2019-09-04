3e23625f234e2e9303ca99e1d06b107e16b393c5
Jamie Sanson <jamie.sanson@trademe.co.nz>
2019-09-05 09:40:04 +1200

    Adds documentation to ConfigDelegate and AppConfig example

--- konfigure/src/main/java/nz/co/trademe/konfigure/api/ConfigDelegate.kt
+++ konfigure/src/main/java/nz/co/trademe/konfigure/api/ConfigDelegate.kt
@@ -2,4 +2,7 @@ package nz.co.trademe.konfigure.api
 
 import kotlin.properties.ReadWriteProperty
 
+/**
+ * Interface simplifying [ReadWriteProperty] usage for config items
+ */
 interface ConfigDelegate<T: Any>: ReadWriteProperty<ConfigRegistry, T>
