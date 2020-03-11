27203b84bcae1efdba4c7f7096091507b9b370f2
Jamie Sanson <jamie.sanson@trademe.co.nz>
2020-03-12 11:04:00 +1300

    Sets background programmatically to support version of Android <6

--- konfigure-android/src/main/java/nz/co/trademe/konfigure/android/ui/adapter/viewholder/DividerViewHolder.kt
+++ konfigure-android/src/main/java/nz/co/trademe/konfigure/android/ui/adapter/viewholder/DividerViewHolder.kt
@@ -1,8 +1,14 @@
 package nz.co.trademe.konfigure.android.ui.adapter.viewholder
 
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import nz.co.trademe.konfigure.android.R
 
 internal class DividerViewHolder(
     parent: ViewGroup
-) : BaseViewHolder(parent.inflate(R.layout.view_holder_divider))
+) : BaseViewHolder(parent.inflate(R.layout.view_holder_divider)) {
+
+    init {
+        itemView.setBackgroundColor(AppCompatResources.getColorStateList(itemView.context, R.color.color_divider).defaultColor)
+    }
+}
